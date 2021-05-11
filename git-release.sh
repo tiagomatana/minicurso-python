6f0406499150e3af8ec67f40f9690931c52cf0b0
Tiago Mtana <tiago@withleaf.io>
2021-05-11 11:01:40 -0300

    TES

--- git-release.sh
+++ git-release.sh
@@ -6,9 +6,9 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 
 if [ $(git config --get remote.origin.url | grep -E 'git@github.com' | wc -l) -gt 0 ]
 then 
-    repo_full_name=$(git config --get remote.origin.url | grep -Po "(?<=git@github\.com:)(.*?)(?=.git)")
+    export repo_full_name=$(git config --get remote.origin.url | grep -Po "(?<=git@github\.com:)(.*?)(?=.git)")
 else
-    repo_full_name=$(git config --get remote.origin.url | sed 's/.*:\/\/github.com\///;s/.git$//')
+    export repo_full_name=$(git config --get remote.origin.url | sed 's/.*:\/\/github.com\///;s/.git$//')
 fi
 
 
@@ -28,5 +28,4 @@ generate_post_data()
 EOF
 }
 
-echo "Create release $version for repo: $repo_full_name branch: $branch"
-/usr/bin/curl -X POST -H "Accept: application/vnd.github.v3+json;" -H "Authorization: token $token" -d "$( generate_post_data )" https://api.github.com/repos/$repo_full_name/releases
+generate_post_data
--- git-version.sh
+++ git-version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export TAG=""
-if [ $(git log -1 --pretty=%B | grep -E ^feature\: | wc -l) -gt 0 ]; then export TAG=$(git semver --next-minor); fi
-if [ $(git log -1 --pretty=%B | grep -E ^breaking\: | wc -l) -gt 0 ]; then export TAG=$(git semver --next-major); fi
-if [ -z $TAG ]; then export TAG=$(git semver --next-patch); fi
+TAG=""
+if [ $(git log -1 --pretty=%B | grep -E ^feature\: | wc -l) -gt 0 ]; then TAG=$(git semver --next-minor); fi
+if [ $(git log -1 --pretty=%B | grep -E ^breaking\: | wc -l) -gt 0 ]; then TAG=$(git semver --next-major); fi
+if [ -z $TAG ]; then TAG=$(git semver --next-patch); fi
 echo $TAG
