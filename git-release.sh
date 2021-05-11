14a3aa8e69401d714e8ebbfa4491efbaa15f416c
Tiago Mtana <tiago@withleaf.io>
2021-05-11 10:35:43 -0300

    feature: testando o release

--- git-release.sh
+++ git-release.sh
@@ -3,8 +3,15 @@
 version=$1
 text=$2
 branch=$(git rev-parse --abbrev-ref HEAD)
-repo_full_name=$(git config --get remote.origin.url | sed 's/.*:\/\/github.com\///;s/.git$//')
-repo_full_name=$(git config --get remote.origin.url | grep -Po "(?<=git@github\.com:)(.*?)(?=.git)")
+
+if [ $(git config --get remote.origin.url | grep -E 'git@github.com' | wc -l) -gt 0 ]
+then 
+    repo_full_name=$(git config --get remote.origin.url | grep -Po "(?<=git@github\.com:)(.*?)(?=.git)")
+else
+    repo_full_name=$(git config --get remote.origin.url | sed 's/.*:\/\/github.com\///;s/.git$//')
+fi
+
+
 token=$(git config --global github.token)
 
 generate_post_data()
@@ -22,4 +29,4 @@ EOF
 }
 
 echo "Create release $version for repo: $repo_full_name branch: $branch"
-curl --data "$(generate_post_data)" "https://api.github.com/repos/$repo_full_name/releases?access_token=$token"
+curl -X POST -H "Accept: application/vnd.github.v3+json;" -H "Authorization: token $token" -d "$( generate_post_data )" https://api.github.com/repos/$repo_full_name/releases
